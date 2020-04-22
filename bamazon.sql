51bbdafd85cde4ec01bd043286c308bd9e904e49
Jennifer Kruk <jenkruk@gmail.com>
2020-04-21 18:45:18 -0700

    table and products created, inquirer started

--- bamazon.sql
+++ bamazon.sql
@@ -3,6 +3,7 @@ CREATE DATABASE bamazon_DB;
 
 USE bamazon_DB;
 
+-- Create table
 CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50) NOT NULL,
@@ -11,3 +12,16 @@ CREATE TABLE products(
   stock_quantity INT default 0,
   PRIMARY KEY (item_id)
 );
+
+-- Insert more than one row at a time
+ INSERT INTO products (product_name, department_name, price, stock_quantity)
+ VALUES ("Toilet Paper", "Essentials", 25.00, 100), 
+ ("Hand Sanitizer", "Essentials", 50.00, 80), 
+ ("Clorox Wipes", "Essentials", 60.00, 90), 
+ ("Hand Soap", "Essentials", 20.00, 200), 
+ ("Wine", "Also Essential", 75.00, 100), 
+ ("Chocolate", "Also Essential", 15.00, 300), 
+ ("Netflix Subscriptions", "Also Essential", 14.00, 400), 
+ ("Puzzles", "Good to Have's", 10.00, 150), 
+ ("Hot Pockets", "Good to Have's", 08.00, 175), 
+ ("Hair Color", "Good to Have's", 12.00, 110);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,13 +12,90 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "password",
+  password: "",
   database: "bamazon_DB"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
-});
+  //Select all products and return the result object:
+  connection.query("SELECT * FROM products", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+
+    // ----------------------------------------------------------------------------------- 
+
+    function placeOrder() {
+      inquirer
+        .prompt({
+          name: "action",
+          type: "list",
+          message: "What is the item id for the product you would like to buy?",
+          choices: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+          ]
+        })
+        .then(function(answer) {
+          switch (answer.action) {
+          case 1:
+            console.log("You are ordering: Toilet Paper")
+            break;
+    
+          case 2:
+            console.log("You are ordering: Hand Sanitizer")
+            break;
+    
+          case 3:
+            console.log("You are ordering: Clorox wipes")
+            break;
+    
+          case 4:
+            console.log("You are ordering: Hand Soap")
+            break;
+      
+          case 5:
+            console.log("You are ordering: Wine")
+            break;
+      
+          case 6:
+            console.log("You are ordering: Chocolate")
+            break;
+
+          case 7:
+            console.log("You are ordering: Netflix Subscription")
+            break;
+      
+          case 8:
+            console.log("You are ordering: Puzzles")
+            break;
+        
+          case 9:
+            console.log("You are ordering: Hot Pockets")
+            break;
+        
+          case 10:
+            console.log("You are ordering: Hair Color")
+            break;
+    
+          default:
+            connection.end();
+            break;
+          };
+        });  //end of then.(function(answer) {switch case}
+        //  prompts.next({
+        //   message: "How many would you like to order?"
+        // });
+      }; //end of placeOrder()
+      placeOrder();
+  }); 
+}); // end of connection.connect
